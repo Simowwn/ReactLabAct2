b33ce0f6c9402577f3cf5bbda10872ae3dcb231b
Simowwn <125945116+Simowwn@users.noreply.github.com>
2024-02-02 12:43:16 +0800

    Updated Data.js

--- src/Data.js
+++ src/Data.js
@@ -2,14 +2,14 @@ export const collectiblesData = [
   {
     photoName: "https://smiski.com/e/wp-content/uploads/2016/09/3_3.png",
     name: "Smiski Bridge",
-    body: "Body Type: Slim",
-    pose: "Pose: Peeking Down",
-    found: "Found: On top of objects and shelves",
+    body: "Body Type: Normal",
+    pose: "Pose: Bridge",
+    found: "Found: Around corners of the room",
     description:
-      "Finds comfort in high places and likes to look down at the scenery below. Is quite weary of things and when it notices something it makes sure to check it out first from a safe distance.",
+      "Always deep in thought. To think flexibly, one must act flexibly! ..which is the reason for this pose.",
   },
   {
-    photoName: "https://smiski.com/e/wp-content/uploads/2016/09/3_3.png",
+    photoName: "https://smiski.com/e/wp-content/uploads/2016/09/3_4.png",
     name: "Smiski Peeking",
     body: "Body Type: Slim",
     pose: "Pose: Peeking Down",
@@ -20,37 +20,37 @@ export const collectiblesData = [
   {
     photoName: "https://smiski.com/e/wp-content/uploads/2016/09/3_3.png",
     name: "Smiski Climbing",
-    body: "Body Type: Slim",
-    pose: "Pose: Climbing",
-    found: "Found: On top of objects and shelves",
+    body: "Body Type: Chubby",
+    pose: "Pose: Rope climbing",
+    found: "Found: Around ropes, strings, cords",
     description:
-      "Enjoys climbing to explore its surroundings. Often seen ascending walls and furniture with ease. Possesses a playful and adventurous nature.",
+      "Curious of what is waiting at the top of the hanging string, this Smiski will climb up without hesitation. However it always has some trouble getting back down. Loves adventure.",
+  },
+  {
+    photoName: "https://smiski.com/e/wp-content/uploads/2016/09/3_6.png",
+    name: "Smiski Little",
+    body: "Body Type: Normal",
+    pose: "Pose: One hand against the wall",
+    found: "Found: Near walls",
+    description:
+      "Little Smiski have made their appearance! For some reason these little ones like to line up against the wall in this pose.",
+  },
+  {
+    photoName: "https://smiski.com/e/wp-content/uploads/2016/09/3_7.png",
+    name: "Smiski Hiding",
+    body: "Body Type: Normal",
+    pose: "Pose: under something",
+    found: "Found: Underneath towels and blankets",
+    description:
+      "Likes to be in small, cosy places. Often found underneath towels and blankets, looking around its surroundings while hiding. Is a little timid in personality.",
   },
   {
     photoName: "https://smiski.com/e/wp-content/uploads/2016/09/3_3.png",
-    name: "Smiski Bridge",
-    body: "Body Type: Slim",
-    pose: "Pose: Peeking Down",
-    found: "Found: On top of objects and shelves",
+    name: "Smiski Handstand",
+    body: "Body Type: Chubby",
+    pose: "Pose: Handstand",
+    found: "Found: Around corners of the room",
     description:
-      "Finds comfort in high places and likes to look down at the scenery below. Is quite weary of things and when it notices something it makes sure to check it out first from a safe distance.",
-  },
-  {
-    photoName: "https://smiski.com/e/wp-content/uploads/2016/09/3_3.png",
-    name: "Smiski Peeking",
-    body: "Body Type: Slim",
-    pose: "Pose: Peeking Down",
-    found: "Found: On top of objects and shelves",
-    description:
-      "Finds comfort in high places and likes to look down at the scenery below. Is quite weary of things and when it notices something it makes sure to check it out first from a safe distance.",
-  },
-  {
-    photoName: "https://smiski.com/e/wp-content/uploads/2016/09/3_3.png",
-    name: "Smiski Climbing",
-    body: "Body Type: Slim",
-    pose: "Pose: Climbing",
-    found: "Found: On top of objects and shelves",
-    description:
-      "Enjoys climbing to explore its surroundings. Often seen ascending walls and furniture with ease. Possesses a playful and adventurous nature.",
+      "Always found doing a handstand against the wall. If you look closely, the peeking Smiski from Series 2 is actually always trying to copy this Smiski.",
   },
 ];
